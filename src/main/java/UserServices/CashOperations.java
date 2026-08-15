1e3ffa9eb69d9a66b96d1a8d01798db523c07f85
Subhayan Dey <subhayandey649@gmail.com>
2026-08-15 19:24:01 +0530

    commit changes

--- src/main/java/UserServices/CashOperations.java
+++ src/main/java/UserServices/CashOperations.java
@@ -8,21 +8,31 @@ import java.sql.SQLException;
 
 public class CashOperations {
 
-    String sql1 = "update users set Amount = amount+? where Username=?";
-    String sql2 = "update users set Amount = amount-? where Username=?";
+    private static final String CREDIT_USER_SQL = "update users set Amount = Amount + ? where Username = ?";
+    private static final String DEBIT_USER_SQL = "update users set Amount = Amount - ? where Username = ? and Amount >= ?";
+    private static final String INSERT_TRANSACTION_SQL = "insert into Transactions(SenderUsername, ReceiverUsername, Amount, TransactionType, TransactionStatus) values (?, ?, ?, ?, ?)";
 
-    public void DepositAmt(String username, int amount)
-    {
+    public void DepositAmt(String username, int amount) {
         try(Connection con = DatabaseConnection.getConnection();
-            PreparedStatement ps = con.prepareStatement(sql1);
+            PreparedStatement ps1 = con.prepareStatement(CREDIT_USER_SQL);
+            PreparedStatement ps2 = con.prepareStatement(INSERT_TRANSACTION_SQL);
         )
         {
-            ps.setInt(1, amount);
-            ps.setString(2, username);
-            int res = ps.executeUpdate();
+            ps1.setInt(1, amount);
+            ps1.setString(2, username);
+            ps2.setString(1, username);
+            ps2.setNull(2, java.sql.Types.VARCHAR);
+            ps2.setInt(3, amount);
+            ps2.setString(4, "Deposit");
+            int res = ps1.executeUpdate();
             if(res>0)
             {
                 System.out.println("Amount has been deposited");
+                ps2.setString(5, "SUCCESS");
+                ps2.executeUpdate();
+            }else{
+                ps2.setString(5, "FAILED");
+                ps2.executeUpdate();
             }
         }catch (SQLException e)
         {
@@ -33,39 +43,70 @@ public class CashOperations {
     public void WithdrawAmt(String username, int amount)
     {
         try(Connection con = DatabaseConnection.getConnection();
-            PreparedStatement ps = con.prepareStatement(sql2);
+            PreparedStatement ps = con.prepareStatement(DEBIT_USER_SQL);
+            PreparedStatement ps2 = con.prepareStatement(INSERT_TRANSACTION_SQL);
         )
         {
             ps.setInt(1, amount);
             ps.setString(2, username);
+            ps.setInt(3, amount);
+            ps2.setString(1, username);
+            ps2.setNull(2, java.sql.Types.VARCHAR);
+            ps2.setInt(3, amount);
+            ps2.setString(4, "Withdraw");
             int res = ps.executeUpdate();
             if(res>0)
             {
                 System.out.println("Amount has been Withdrawn");
+                ps2.setString(5, "SUCCESS");
+                ps2.executeUpdate();
+            }else {
+                ps2.setString(5, "FAILED");
+                ps2.executeUpdate();
             }
         }catch (SQLException e)
         {
             e.printStackTrace();
+
         }
     }
 
     public void TransferAmt(String sender, String receiver ,int amount)
     {
         try(Connection con = DatabaseConnection.getConnection();
-            PreparedStatement ps1 = con.prepareStatement(sql1);
-            PreparedStatement ps2 = con.prepareStatement(sql2);
+            PreparedStatement ps1 = con.prepareStatement(CREDIT_USER_SQL);
+            PreparedStatement ps2 = con.prepareStatement(DEBIT_USER_SQL);
+            PreparedStatement ps3 = con.prepareStatement(INSERT_TRANSACTION_SQL);
         )
         {
-            ps1.setInt(1, amount);
-            ps1.setString(2, receiver);
+            con.setAutoCommit(false);
+
             ps2.setInt(1, amount);
             ps2.setString(2, sender);
-            int res1 =  ps1.executeUpdate();
+            ps2.setInt(3, amount);
             int res2 =  ps2.executeUpdate();
+
+            int res1 = 0;
+            if (res2 > 0) {
+                ps1.setInt(1, amount);
+                ps1.setString(2, receiver);
+                res1 = ps1.executeUpdate();
+            }
+
+            ps3.setString(1, sender);
+            ps3.setString(2, receiver);
+            ps3.setInt(3, amount);
+            ps3.setString(4, "Transfer");
 
             if(res1>0 && res2>0)
             {
                 System.out.println("Amount has been transferred");
+                ps3.setString(5, "SUCCESS");
+                ps3.executeUpdate();
+                con.commit();
+            }else  {
+                con.rollback();
+                System.out.println("Amount could not be transferred");
             }
         }catch (SQLException e)
         {
